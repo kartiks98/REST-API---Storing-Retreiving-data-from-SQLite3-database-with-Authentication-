9e280f17ad1e80172bbfab4e9b9e2e514029a5a4
K S <kartiksaini135@gmail.com>
2020-05-02 14:23:10 +0530

    REST API - storing & retreiving data in database without using SQLite queries (including stores)

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyData.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
